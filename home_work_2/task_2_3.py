34ff77d22c7c4ccdf9e8adbf046ed13578b1c26b
NikitaPanferov <nikpanfer@gmail.com>
2021-02-24 22:30:21 +0300

    переписал задачу 3

--- home_work_2/task_2_3.py
+++ home_work_2/task_2_3.py
@@ -5,39 +5,19 @@
 Под основным кодом оставляю код, удовлетворяющий условию написанному выше
 """
 begin_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-i = 0
-while i < len(begin_list):
-    if '+' in begin_list[i] or '-' in begin_list[i]:
-        if len(begin_list[i]) == 2:
-            begin_list[i] = f'{begin_list[i][0]}{int(begin_list[i][1]):02d}'
-    elif begin_list[i].isdigit():
-        if len(begin_list[i]) == 1:
-            begin_list[i] = f'{int(begin_list[i][0]):02d}'
-    else:
-        i += 1
-        continue
-    begin_list.insert(i, '"')
-    i += 2
-    begin_list.insert(i, '"')
-    i += 1
 
-alf = '1234567890+-'
-js = begin_list[0]
-for i in range(1, len(begin_list)):
-    if (begin_list[i-1] == '"' and begin_list[i][0] in alf) or (begin_list[i-1][0] in alf and begin_list[i] == '"'):
-        js += begin_list[i]
-    else:
-        js += ' ' + begin_list[i]
-print(js)
+for i, el in reversed(list(enumerate(begin_list))):
+    el = el.replace('+' or '-', '')
+    if el.isdigit():
+        begin_list.insert(i + 1, '"')
+        begin_list[i] = begin_list[i].replace(el, f'{int(el):02}')
+        begin_list.insert(i, '"')
 
-"""
-begin_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-for i in range(len(begin_list)):
-    if begin_list[i][0] == '+' or begin_list[i][0] == '-':
-        if len(begin_list[i]) > 1:
-            begin_list[i] = f'"{begin_list[i][0]}{int(begin_list[i][1]):02d}"'
-    if begin_list[i].isdigit():
-        begin_list[i] = f'"{int(begin_list[i]):02d}"'
 js = ' '.join(begin_list)
+js = js.split('"')
+for i in range(1, len(js), 2):
+    js[i] = js[i].replace(' ', '"')
+js = ''.join(js)
 print(js)
-"""
+
+
